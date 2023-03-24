39eae239fb6b35a80226a699cc804e69d4bf1093
dinghq.alex <dinghq.alex@gmail.com>
2023-03-24 18:31:53 +0800

    添加高斯模糊的效果
    Signed-off-by: dinghq.alex <dinghq.alex@gmail.com>

---
+++ Assets/Code/Core/PostEffects/GaussianBlur.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+namespace GameLib.Core.PostEffect
+{
+    /// <summary>
+    /// 后处理的高斯模糊
+    /// </summary>
+    public class GaussianBlur : PostEffectsBase
+    {
+        public Shader gaussianBlurShader;
+        private Material gaussianBlurMaterial = null;
+
+        public Material material
+        {
+            get
+            {
+                gaussianBlurMaterial = CheckShaderAndCreateMaterial(gaussianBlurShader, gaussianBlurMaterial);
+                return gaussianBlurMaterial;
+            }
+        }
+
+        /// <summary>
+        /// 调整高斯模糊迭代次数，模糊范围和缩放系数
+        /// </summary>
+        [Range(0, 4)]
+        public int iterations = 3;
+
+        /// <summary>
+        /// 在高斯核维数不变的情况下，blursize越大，模糊程度越高，但是采样数不会受影响
+        /// </summary>
+        [Range(0.2f, 3.0f)]
+        public float blurSpread = 0.6f;
+
+        /// <summary>
+        /// 值越大，需要处理的像素越少，也能进一步提高模糊程度，这个数值太大的话可能会让图形像素化
+        /// </summary>
+        [Range(1, 8)]
+        public int downSample = 2;
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            //利用两个临时缓存在迭代之间进行交替
+            if (material != null)
+            {
+                int rtW = source.width / downSample;
+                int rtH = source.height / downSample;
+
+                //定义buffer0缓存
+                RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
+                buffer0.filterMode = FilterMode.Bilinear;
+                //把src的图形缩放后放在buffer0中
+                Graphics.Blit(source, buffer0);
+                //在执行第一个Pass的时候，输入是buffer0，输出是buffer1，完毕之后先把buffer0释放，
+                //再把结果值buffer1存储到buffer0中，重新分配buffer1，然后再调用第二个Pass
+                for (int i = 0; i < iterations; i++)
+                {
+                    material.SetFloat("_BlurSize", 1.0f + i * blurSpread);
+
+                    RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+                    // Render the vertical pass
+                    Graphics.Blit(buffer0, buffer1, material, 0);
+
+                    RenderTexture.ReleaseTemporary(buffer0);
+                    buffer0 = buffer1;
+                    buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+                    // Render the horizontal pass
+                    Graphics.Blit(buffer0, buffer1, material, 1);
+
+                    RenderTexture.ReleaseTemporary(buffer0);
+                    buffer0 = buffer1;
+                }
+                //上面的迭代完成之后buffer0存储的是最终的图像，再通过Graphics.Blit把结果显示到屏幕上并释放缓存
+                Graphics.Blit(buffer0, destination);
+                RenderTexture.ReleaseTemporary(buffer0);
+            }
+            else
+            {
+                Graphics.Blit(source, destination);
+            }
+        }
+    }
+}
---
+++ Assets/GameAssets/Resources/Graphics/Shader/7_PostEffects/GaussianBlur.shader
@@ -0,0 +1,102 @@
+﻿/**
+    后处理效果
+	高斯模糊
+*/
+Shader "GameLib/PostEffects/GaussianBlur"
+{
+    Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_BlurSize ("Blur Size", Float) = 1.0
+	}
+	SubShader {
+
+		/**
+			unity会把 CGINCLUDE 和 ENDCG 之间的代码插入到每一个pass中，已达到声明一遍，多次使用的目的。
+			（参考）例如，可以在 CGINCLUDE 和 ENDCG 之间 定义多个 顶点和片段方法，在pass里只要写明 #pragma vertex 顶点方法名 #pragma fragment 片段方法名 即可，而不用写具体的函数实现
+			链接：https://www.jianshu.com/p/2d4b4ae57c12
+			https://www.cnblogs.com/fengxing999/p/10012256.html
+		*/
+		CGINCLUDE
+		
+		#include "UnityCG.cginc"
+		
+		sampler2D _MainTex;  
+		half4 _MainTex_TexelSize;
+		float _BlurSize;
+		  
+		struct v2f {
+			float4 pos : SV_POSITION;
+			half2 uv[5]: TEXCOORD0;
+		};
+		  
+		v2f vertBlurVertical(appdata_img v) {
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+			
+			half2 uv = v.texcoord;
+			
+			o.uv[0] = uv;
+			o.uv[1] = uv + float2(0.0, _MainTex_TexelSize.y * 1.0) * _BlurSize;
+			o.uv[2] = uv - float2(0.0, _MainTex_TexelSize.y * 1.0) * _BlurSize;
+			o.uv[3] = uv + float2(0.0, _MainTex_TexelSize.y * 2.0) * _BlurSize;
+			o.uv[4] = uv - float2(0.0, _MainTex_TexelSize.y * 2.0) * _BlurSize;
+					 
+			return o;
+		}
+		
+		v2f vertBlurHorizontal(appdata_img v) {
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+			
+			half2 uv = v.texcoord;
+			
+			o.uv[0] = uv;
+			o.uv[1] = uv + float2(_MainTex_TexelSize.x * 1.0, 0.0) * _BlurSize;
+			o.uv[2] = uv - float2(_MainTex_TexelSize.x * 1.0, 0.0) * _BlurSize;
+			o.uv[3] = uv + float2(_MainTex_TexelSize.x * 2.0, 0.0) * _BlurSize;
+			o.uv[4] = uv - float2(_MainTex_TexelSize.x * 2.0, 0.0) * _BlurSize;
+					 
+			return o;
+		}
+		
+		fixed4 fragBlur(v2f i) : SV_Target {
+			float weight[3] = {0.4026, 0.2442, 0.0545};
+			
+			fixed3 sum = tex2D(_MainTex, i.uv[0]).rgb * weight[0];
+			
+			for (int it = 1; it < 3; it++) {
+				sum += tex2D(_MainTex, i.uv[it*2-1]).rgb * weight[it];
+				sum += tex2D(_MainTex, i.uv[it*2]).rgb * weight[it];
+			}
+			
+			return fixed4(sum, 1.0);
+		}
+		    
+		ENDCG
+		
+		ZTest Always Cull Off ZWrite Off
+		
+		Pass {
+			NAME "GAUSSIAN_BLUR_VERTICAL"
+			
+			CGPROGRAM
+			  
+			#pragma vertex vertBlurVertical  
+			#pragma fragment fragBlur
+			  
+			ENDCG  
+		}
+		
+		Pass {  
+			NAME "GAUSSIAN_BLUR_HORIZONTAL"
+			
+			CGPROGRAM  
+			
+			#pragma vertex vertBlurHorizontal  
+			#pragma fragment fragBlur
+			
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
