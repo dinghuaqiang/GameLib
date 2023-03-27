463d5c4164fba0ca25ee1b2491977ea642dd52b4
dinghq.alex <dinghq.alex@gmail.com>
2023-03-27 11:52:55 +0800

    测试Bloom效果

---
+++ Assets/Code/Core/PostEffects/Bloom.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+namespace GameLib.Core.PostEffect
+{
+    /// <summary>
+    /// Bloom效果
+    /// </summary>
+    public class Bloom : PostEffectsBase
+    {
+        public Shader bloomShader;
+        private Material bloomMaterial = null;
+
+        public Material material
+        {
+            get
+            {
+                bloomMaterial = CheckShaderAndCreateMaterial(bloomShader, bloomMaterial);
+                return bloomMaterial;
+            }
+        }
+
+        /// <summary>
+        /// 调整高斯模糊迭代次数，模糊范围和缩放系数
+        /// </summary>
+        [Range(0, 4)]
+        public int iterations = 3;
+
+        /// <summary>
+        /// 在高斯核维数不变的情况下，blursize越大，模糊程度越高，但是采样数不会受影响
+        /// </summary>
+        [Range(0.2f, 3.0f)]
+        public float blurSpread = 0.6f;
+
+        /// <summary>
+        /// 值越大，需要处理的像素越少，也能进一步提高模糊程度，这个数值太大的话可能会让图形像素化
+        /// </summary>
+        [Range(1, 8)]
+        public int downSample = 2;
+
+        /// <summary>
+        /// 亮度阈值，控制较亮区域时使用的阈值大小
+        /// 绝大多数情况下，图像的亮度值不会超过1.如果开启了HDR，硬件会允许把颜色值存储在一个更高精度范围的缓冲，此时像素的亮度值可能就会超过1.
+        /// 所以这里把值限定在0-4
+        /// </summary>
+        [Range(0.0f, 4.0f)]
+        public float luminanceThreshold = 0.6f;
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            //利用两个临时缓存在迭代之间进行交替
+            if (material != null)
+            {
+                material.SetFloat("_LuminanceThreshold", luminanceThreshold);
+
+                int rtW = source.width / downSample;
+                int rtH = source.height / downSample;
+
+                //定义buffer0缓存
+                RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
+                buffer0.filterMode = FilterMode.Bilinear;
+                //使用Shader的第一个Pass提取图像中较亮的区域，将提取到的较亮的区域存储在buffer0中
+                Graphics.Blit(source, buffer0, material, 0);
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    material.SetFloat("_BlurSize", 1.0f + i * blurSpread);
+
+                    RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+                    // Render the vertical pass
+                    Graphics.Blit(buffer0, buffer1, material, 1);
+
+                    RenderTexture.ReleaseTemporary(buffer0);
+                    buffer0 = buffer1;
+                    buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+                    // Render the horizontal pass
+                    Graphics.Blit(buffer0, buffer1, material, 2);
+
+                    RenderTexture.ReleaseTemporary(buffer0);
+                    buffer0 = buffer1;
+                }
+                //将模糊后较亮区域存储在了buffer0中，再传递给材质中的_Bloom纹理属性
+                material.SetTexture("_Bloom", buffer0);
+                //使用第4个Pass来进行最后的混合，并将结果存储在目标渲染纹理destination中
+                Graphics.Blit(source, destination, material, 3);
+                //释放临时内存
+                RenderTexture.ReleaseTemporary(buffer0);
+            }
+            else
+            {
+                Graphics.Blit(source, destination);
+            }
+        }
+    }
+}
---
+++ Assets/GameAssets/Resources/Graphics/Shader/7_PostEffects/Bloom.shader
@@ -0,0 +1,120 @@
+﻿/**
+    后处理效果
+	高斯模糊
+*/
+Shader "GameLib/PostEffects/Bloom"
+{
+    Properties {
+		//输入的RenterTexture
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		//高斯模糊后的较亮区域
+		_Bloom ("Bloom (RGB)", 2D) = "black" {}
+		//提取较亮区域使用的阈值
+		_LuminanceThreshold ("Luminance Threshold", Float) = 0.5
+		//控制不同迭代之间高斯模糊的模糊区域范围
+		_BlurSize ("Blur Size", Float) = 1.0
+	}
+	SubShader {
+		/**
+			unity会把 CGINCLUDE 和 ENDCG 之间的代码插入到每一个pass中，已达到声明一遍，多次使用的目的。
+			（参考）例如，可以在 CGINCLUDE 和 ENDCG 之间 定义多个 顶点和片段方法，在pass里只要写明 #pragma vertex 顶点方法名 #pragma fragment 片段方法名 即可，而不用写具体的函数实现
+			链接：https://www.jianshu.com/p/2d4b4ae57c12
+			https://www.cnblogs.com/fengxing999/p/10012256.html
+		*/
+		CGINCLUDE
+		
+		#include "UnityCG.cginc"
+		
+		sampler2D _MainTex;  
+		//得到相邻像素的纹理坐标偏移量
+		half4 _MainTex_TexelSize;
+		sampler2D _Bloom;
+		float _LuminanceThreshold;
+		float _BlurSize;
+		  
+		struct v2f {
+			float4 pos : SV_POSITION;
+			half2 uv: TEXCOORD0;
+		};
+		  
+		v2f vertExtractBright(appdata_img v) {
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+			o.uv = v.texcoord;
+			return o;
+		}
+
+		fixed luminance(fixed4 color)
+		{
+			return 0.2125 * color.r + 0.7154 * color.g + 0.0721 * color.b;
+		}
+
+		fixed4 fragExtractBright(v2f i) : SV_Target
+		{
+			fixed4 color = tex2D(_MainTex, i.uv);
+			//将采样得到的亮度值减去阈值，并把结果接取到0-1范围内
+			fixed val = clamp(luminance(color) - _LuminanceThreshold, 0.0, 1.0);
+			//得到提取后的亮部区域
+			return color * val;
+		}
+		
+		struct v2fBloom
+		{
+			float4 pos : SV_POSITION;
+			half4 uv : TEXCOORD0;
+		};
+
+		v2fBloom vertBloom(appdata_img v)
+		{
+			v2fBloom o;
+
+			o.pos = UnityObjectToClipPos(v.vertex);
+			//xy分量对应_MainTex原图像的纹理坐标
+			o.uv.xy = v.texcoord;
+			//zw分量是Bloom，模糊后的较亮区域的纹理坐标
+			o.uv.zw = v.texcoord;
+
+			#if UNITY_UV_STARTS_AT_TOP
+			if (_MainTex_TexelSize.y < 0.0)
+				o.uv.w = 1.0 - o.uv.w;
+			#endif
+
+			return o;
+		}
+
+		fixed4 fragBloom(v2fBloom i) : SV_Target
+		{
+			//把两张纹理采样结果相加进行混合
+			return tex2D(_MainTex, i.uv.xy) + tex2D(_Bloom, i.uv.zw);
+		}
+		    
+		ENDCG
+		
+		//定义4个Pass
+		ZTest Always Cull Off ZWrite Off
+		
+		Pass {
+			CGPROGRAM
+			  
+			#pragma vertex vertExtractBright  
+			#pragma fragment fragExtractBright
+			  
+			ENDCG  
+		}
+
+		//使用高斯模糊的Pass，Unity内部会把所有的Pass的Name转换成大写，使用UsePass的时候要注意PassName的书写
+		UsePass "GameLib/PostEffects/GaussianBlur/GAUSSIAN_BLUR_VERTICAL"
+		
+		UsePass "GameLib/PostEffects/GaussianBlur/GAUSSIAN_BLUR_HORIZONTAL"
+		
+		Pass {
+			CGPROGRAM  
+			
+			#pragma vertex vertBloom  
+			#pragma fragment fragBloom
+			
+			ENDCG
+		}
+	} 
+	FallBack Off
+}
